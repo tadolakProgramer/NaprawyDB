581a1cb7a56e8fd72ea96c284f4d16c674d6376b
Tadeusz <tadolak@gmail.com>
2019-12-04 11:41:19 +0100

    Initial commit

---
+++ src/main/java/com/tadolak/NaprawyDB/NaprawyDbApplication.java
@@ -0,0 +1,13 @@
+package com.tadolak.NaprawyDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaprawyDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaprawyDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadolak/NaprawyDB/NaprawyDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadolak.NaprawyDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaprawyDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
